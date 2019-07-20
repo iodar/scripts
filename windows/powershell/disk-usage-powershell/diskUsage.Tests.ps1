4775c3aece67c729127aca7d91fa112f10d7f08d
iodar <granzowdario@gmail.com>
2019-07-21 00:23:17 +0200

    added testdata and tests

---
+++ windows/powershell/disk-usage-powershell/diskUsage.Tests.ps1
@@ -0,0 +1,32 @@
+# testdata
+$notEmptyFolderPath = ".\testdata\not-empty-folder"
+$emptyFolderPath = ".\testdata\empty-folder"
+
+. .\diskUsage.ps1
+
+Describe "get-DiskUsageInBytes" {
+    It "if folder not empty => calcs the correct amount of bytes in folder" {
+        $sizeOfTestdataFolder = get-DiskUsageInBytes -folder $notEmptyFolderPath
+        $sizeOfTestdataFolder | Should -BeExactly 68448
+    }
+    
+    It "if folder empty => calcs the correct amount of bytes in folder" {
+        $sizeOfTestdataFolder = get-DiskUsageInBytes -folder $emptyFolderPath
+        $sizeOfTestdataFolder | Should -BeExactly 0
+    }
+
+    It "if not folder given => uses the current folder to calc size" {
+        # FIXME: There has to be a better way        
+        # save current location for later use -> otherwise the terminal will terminate in
+        # another location than intinally started (very annoying)
+        $initalLocation = $pwd.Path
+        # change location to testdata folder
+        Set-Location -Path $notEmptyFolderPath
+        $sizeOfTestdataFolder = get-DiskUsageInBytes
+        $sizeOfTestdataFolder | Should -BeExactly 68448
+        # return to location
+        Set-Location -Path $initalLocation
+    }
+}
+
+# TODO: add tests for get-UsageInCustomUnit
