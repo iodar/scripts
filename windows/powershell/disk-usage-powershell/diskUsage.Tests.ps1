c046dcced683bd4915cee59411faeead5e4ffe00
iodar <granzowdario@gmail.com>
2019-07-21 15:28:10 +0200

    chnaged testst

--- windows/powershell/disk-usage-powershell/diskUsage.Tests.ps1
+++ windows/powershell/disk-usage-powershell/diskUsage.Tests.ps1
@@ -31,27 +31,27 @@ Describe "get-DiskUsageInBytes" {
 }
 
 Describe "get-UsageInCustomUnit" {
-    It "no unit given => prints size in bytes" {
+    It "no unit given => calculates size in bytes" {
         $sizeOfTestdataFolder = get-UsageInCustomUnit -folder $notEmptyFolderPath
         $sizeOfTestdataFolder | Should -BeExactly 68448
     }
 
-    It "unit 'BYTES' given => prints size in bytes" {
+    It "unit 'BYTES' given => calculates size in bytes" {
         $sizeOfTestdataFolder = get-UsageInCustomUnit -folder $notEmptyFolderPath -unit "BYTES"
         $sizeOfTestdataFolder | Should -BeExactly 68448
     }
     
-    It "unit 'KILO_BYTES' given => prints size in Kbytes" {
+    It "unit 'KILO_BYTES' given => calculates size in Kbytes" {
         $sizeOfTestdataFolder = get-UsageInCustomUnit -folder $notEmptyFolderPath -unit "KILO_BYTES"
         $sizeOfTestdataFolder | Should -BeExactly 67
     }
     
-    It "unit 'MEGA_BYTES' given => prints size in Mbytes" {
+    It "unit 'MEGA_BYTES' given => calculates size in Mbytes" {
         $sizeOfTestdataFolder = get-UsageInCustomUnit -folder $notEmptyFolderPath -unit "MEGA_BYTES"
         $sizeOfTestdataFolder | Should -BeExactly 0
     }
     
-    It "unit 'GIGA_BYTES' given => prints size in Gbytes" {
+    It "unit 'GIGA_BYTES' given => calculates size in Gbytes" {
         $sizeOfTestdataFolder = get-UsageInCustomUnit -folder $notEmptyFolderPath -unit "GIGA_BYTES"
         $sizeOfTestdataFolder | Should -BeExactly 0
     }
