675de0aea7f9d1c5e0516c968f9016cd725f4484
iodar <granzowdario@gmail.com>
2019-07-21 19:52:48 +0200

    changed logic of runTest.ps1 and added test

--- windows/powershell/disk-usage-powershell/runTest.ps1
+++ windows/powershell/disk-usage-powershell/runTest.ps1
@@ -1,1 +1,20 @@
-Invoke-Pester
+function get-installedPesterVersion {
+    return Get-InstalledModule -Name Pester | Select-Object Version
+}
+
+function runPesterTests {
+    $recommandedPersterMajorVersion = "4"
+    $installedPesterVersion = get-installedPesterVersion
+    $installedPersterMajorVersion =$installedPesterVersion.Version.Major
+    
+    if ($installedPersterMajorVersion -lt $recommandedPersterMajorVersion) {
+        Write-Host -ForegroundColor DarkYellow -Object "Your Pester version is `"$($installedPesterVersion.toString())`". Major Version $recommandedPersterMajorVersion is recommanded."
+    } else {
+        # Clear-Host
+        # Write-Host "Running tests ... `n" -ForegroundColor Cyan
+        # Invoke-Pester
+    }
+
+}
+
+runPesterTests
